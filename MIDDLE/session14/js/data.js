5e2eb7409e6b05e74a461a0a957322818dcfd5cd
7art <sem85cv@yandex.ru>
2019-01-27 12:28:59 +0200

    init v1 (using e.path)

---
+++ MIDDLE/session14/js/data.js
@@ -0,0 +1,102 @@
+
+var data = [{
+	url: "desktopwallpapers.org.ua/mini/201203/14858.jpg",
+	name: "CHEVROLET",
+	id : 1,
+	params: {
+		status: true,
+		progress: "80"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1422153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201505/39933.jpg",
+	name: "INFINITI",
+	id : 2,
+	params: {
+		status: true,
+		progress: "88"
+	},
+	description : "Lorem ipsum dolor sit amet",
+	date : 1421153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201510/41430.jpg",
+	name: "VOLKSWAGEN",
+	id : 3,
+	params: {
+		status: false,
+		progress: "64"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1426153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201108/3747.jpg",
+	name: "FERRARI",
+	id : 4,
+	params: {
+		status: true,
+		progress: "38"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1428153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201307/27834.jpg",
+	name: "BMW",
+	id : 5,
+	params: {
+		status: true,
+		progress: "12"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1402153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201212/21975.jpg",
+	name: "MERCEDESS",
+	id : 6,
+	params: {
+		status: true,
+		progress: "83"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1442153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201505/39829.jpg",
+	name: "SKODA",
+	id : 7,
+	params: {
+		status: true,
+		progress: "49"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1482153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201311/30417.jpg",
+	name: "FORD",
+	id : 8,
+	params: {
+		status: true,
+		progress: "14"
+	},
+	description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+	date : 1442153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201203/14766.jpg",
+	name: "TOYOTA",
+	id : 9,
+	params: {
+		status: true,
+		progress: "20"
+	},
+	description : "Lorem ipsum dolor sit amet",
+	date : 1322153200637
+},{
+	url: "desktopwallpapers.org.ua/mini/201503/39582.jpg",
+	name: "RENAULT",
+	id : 10,
+	params: {
+		status: true,
+		progress: "40"
+	},
+	description : "Lorem ipsum dolor sit amet",
+	date : 1322159200637
+}]
---
+++ MIDDLE/session14/js/main.js
@@ -0,0 +1,118 @@
+(function() {
+  "use strict";
+  const btn = document.getElementById("play"),
+    mainDiv = document.getElementById("mainGallery"),
+    typeSelector = document.getElementById("sort-type"),
+    counter = document.querySelector("#counter"),
+    message = document.querySelector(".message");
+  let hiddenGalleryItems = prepareData(data);
+  let displayedGalleryItems = [];
+
+  //обрезаем строку
+  function cutString(str) {
+    return str.length >= 26 ? str.substring(0, 26) : str;
+  }
+
+  //добавляем http
+  function addHttp(str) {
+    return str.startsWith("http://") ? str : "http://" + str;
+  }
+
+  //преобразование даты
+  function formatDate(date) {
+    return moment(date).format("YYYY/MM/DD HH:mm");
+  }
+
+  //делаем первую букву большой, остальные маленькие
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+
+  //выводим сообщение
+  function showMessage(block, message) {
+    block.innerHTML = message;
+    setTimeout(function() {
+      block.innerHTML = "";
+    }, 2500);
+  }
+
+  //формируем и отображаем галерею
+  function buildGallery(array) {
+    let resultHtml = "";
+    toggleButton(array);
+    array.forEach(item => {
+      resultHtml += `<div class="col-md-3 col-sm-4 col-xs-6 text-center">
+      <div class="thumbnail">
+        <img src="${item.url}" alt="${item.name}"> 
+        <div class="caption">
+          <h3>${item.id}: ${item.name}</h3>
+          <p>${item.description}</p\
+          <p>${item.date}</p>
+        </div>
+        <button class="btn btn-danger" >
+        Удалить
+        </button>
+      </div>
+    </div>`;
+    });
+    mainDiv.innerHTML = resultHtml;
+    counter.innerHTML = array.length;
+  }
+
+  //получаем результирующий массив объектов
+  function prepareData(data) {
+    // let limit;
+    // if (+lineSelector.value) limit = lineSelector.value * 3;
+    // if (limit) data = data.slice(0, limit);
+
+    return data.map(item => {
+      return {
+        url: addHttp(item.url),
+        id: item.id,
+        name: item.name,
+        description: cutString(item.description),
+        date: formatDate(item.date)
+      };
+    });
+  }
+
+  //деактивируем/активируем кнопку
+  function toggleButton(array) {
+    if (data.length === array.length) {
+      btn.setAttribute("disabled", true);
+    } else {
+      btn.removeAttribute("disabled");
+    }
+  }
+
+  //при нажатии на btn показываем один элемент из массива hiddenGalleryItems
+  function showOneItem() {
+    //добавляем первый обьэкт из массива hiddenGalleryItems в массив displayedGalleryItems
+    displayedGalleryItems = displayedGalleryItems.concat(
+      hiddenGalleryItems.splice(0, 1)
+    );
+
+    buildGallery(displayedGalleryItems);
+  }
+
+  //при нажатии на btn удаляем один элемент из массива displayedGalleryItems
+  function removeOneItem(e) {
+   // console.log(e);
+    // находим объєкт с помощью атрибута src в массиве displayedGalleryItems 
+    let findItem = displayedGalleryItems.filter(item => {
+      return item.url === e.path[1].childNodes[1].src; 
+    });
+    // добавляєм его в массив hiddenGalleryItems
+    hiddenGalleryItems = hiddenGalleryItems.concat(findItem); 
+
+    //удаляем объєкт из массива displayedGalleryItems
+    displayedGalleryItems = displayedGalleryItems.filter(item => {
+      return item.url !== e.path[1].childNodes[1].src;
+    });
+
+    buildGallery(displayedGalleryItems);
+  }
+
+  mainDiv.addEventListener("click", removeOneItem);
+  btn.addEventListener("click", showOneItem);
+})();
