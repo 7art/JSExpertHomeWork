6602e164d753233faa347e8a6cc912ee2812896a
7art <sem85cv@yandex.ru>
2019-02-07 01:56:51 +0200

    added password timer

---
+++ MIDDLE/session16/js/service.js
@@ -0,0 +1,26 @@
+(function () {
+    const passwordField = document.querySelector("#inputPassword");
+    const logBtn = document.querySelector("#signin");
+
+    let oldValue = "";
+
+    let hidePassChar = function () {
+        oldValue += passwordField.value.substring(oldValue.length, passwordField.value.length);
+        setTimeout(() => {
+            let newValue = "";
+            for (let i = 0; i < passwordField.value.length; i++) {
+                newValue += "*";
+            }
+            passwordField.value = newValue;
+        }, 1000);
+    }
+
+    let setAndClear = function () {
+        console.log(oldValue);
+        passwordField.value = oldValue;
+        oldValue = "";
+    }
+    
+    passwordField.addEventListener("input", hidePassChar);
+    logBtn.addEventListener("click", setAndClear);
+})()
